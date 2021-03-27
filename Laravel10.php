602e3bfc8e45db929902505b9cdb433523792174
kazuma <cazuma11@yahoo.co.jp>
2021-03-27 14:16:36 +0000

    Laravel10課題

---
+++ Laravel10.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--課題４add Action-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"
+content="IE=edge">
+        <meta name="viewport" content="width=device-
+        width, initial-scale=1">
+        
+        <title>Profile</title>
+    </head>
+    <body>
+        <h1>add Action</h1>
+    </body>
+</html>
+
+<!DOCTYPE html>
+<!--課題４edit Action-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"
+content="IE=edge">
+        <meta name="viewport" content="width=device-
+        width, initial-scale=1">
+        
+        <title>Profile</title>
+    </head>
+    <body>
+        <h1>edit Action</h1>
+    </body>
+</html>
